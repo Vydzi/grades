4177b57106a51cb019d197ed84cdd25596f440d2
Vydzi <101144771+Vydzi@users.noreply.github.com>
2022-09-19 20:05:38 +0200

    Create tridaData.cs

---
+++ tridaData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_grades
+{
+    internal class tridaData
+    {
+        public static string[] jmenaPredmetu = { "Čj", "Aj", "Hw", "ALP", "OS", "M", "D", "ZEL", "Fy", "Ze", "IKT", "OPŘ" };
+        public static string[] jmenaZakuA = { "Lumír Mandík vůdce", "Vimmer J1rk0s", "Dáda Odstričil", "Šindy Jan", "Fehl Petr", "Mráček Martin", "Gegel Honza", "Masový muž", "´Hromádko Marek", "Kohout Marek", "Baťa Petr", "Frank Adam", "Vitásek Jakub", "Vrátil Josef", "Pajá Pavlový", "Lukáš Tečka",  };
+        public static string[] jmenaZakuB = { "Áhmed Robin černokožec", "Belanec Jakub", "Bláha kokot Jan", "Bombera zabij se Mojmír", "Buneš Váša", "Fremund Jiří", "Jupa píčus Tadeáš", "Schuller Matyáš", "Heřmánek autista Petr","Stefan Petr","Nguyen žlutokožec Antonín", "Vítr Marek", "Váňa Jan", "Zelenka Filip", "Šimandl Marcel", "Uher Pvel", "Vydrářová Veronika" };
+    }
+}
